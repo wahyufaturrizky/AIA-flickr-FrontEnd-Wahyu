e898c9b940b045b938215374cd64f7d2905934ec
wahyu fatur rizki <wahyu.rizky@docdoc.com>
2022-06-23 09:09:02 +0700

    Updates

---
+++ precache-manifest.8cf6b90938282fc5f0bb731c5c5bc676.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dee3738ab88946900c375e751ab0672",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56d4104f96fbfa3abb4e",
+    "url": "/static/css/main.2b9e7fba.chunk.css"
+  },
+  {
+    "revision": "b810c73796efe9ba24c2",
+    "url": "/static/js/2.7bcbe598.chunk.js"
+  },
+  {
+    "revision": "56d4104f96fbfa3abb4e",
+    "url": "/static/js/main.d2b98097.chunk.js"
+  },
+  {
+    "revision": "9d358751c214f5d382e2",
+    "url": "/static/js/runtime-main.ddb703dd.js"
+  },
+  {
+    "revision": "cea2bf042d83c835aaaa86accaf45a32",
+    "url": "/static/media/flickr-loading.cea2bf04.gif"
+  },
+  {
+    "revision": "b9c8ddfc05ad9301c69ed353456ba17b",
+    "url": "/static/media/flickr.b9c8ddfc.png"
+  },
+  {
+    "revision": "3bd2600df5b5b7914fb817f2344fb5df",
+    "url": "/static/media/proxima-nova-light.3bd2600d.woff2"
+  },
+  {
+    "revision": "c07517329a30d7a4d303f797171c7def",
+    "url": "/static/media/proxima-nova-regular.c0751732.woff2"
+  },
+  {
+    "revision": "9b6f2d8ae1e4e8dec8ee1cde27e3259a",
+    "url": "/static/media/proxima-nova-semibold.9b6f2d8a.woff2"
+  }
+]);
